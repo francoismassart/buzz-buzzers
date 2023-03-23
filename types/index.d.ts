3e5863aa093c256961fb165e1e776b4d00391469
francoismassart <francois.massart@gmail.com>
2023-03-23 17:57:58 +0100

    fix: type definition with optional params

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 import { IBuzzer } from "./types";
 declare const buzzer: (
-  singleMode: boolean,
-  reduceSetLeds: boolean
+  singleMode?: boolean,
+  reduceSetLeds?: boolean
 ) => IBuzzer | IBuzzer[];
 export default buzzer;
